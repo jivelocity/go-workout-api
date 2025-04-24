655fb553e3d2263e51b4b0623d9ce060ac8b8fea
Rajib Aqil Akbar <rajibaqil11@gmail.com>
2025-04-24 15:36:29 +0700

    initial commit

---
+++ internal/app/app.go
@@ -0,0 +1,43 @@
+package app
+
+import (
+	"database/sql"
+	"fmt"
+	"log"
+	"net/http"
+	"os"
+
+	"github.com/jivelocity/femGo/internal/api"
+	"github.com/jivelocity/femGo/internal/store"
+)
+
+type Application struct {
+	Logger         *log.Logger
+	WorkoutHandler *api.WorkoutHandler
+	DB             *sql.DB
+}
+
+func NewApplication() (*Application, error) {
+	pgDB, err := store.Open()
+	if err != nil {
+		return nil, err
+	}
+	logger := log.New(os.Stdout, "", log.Ldate|log.Ltime)
+
+	// our store will go here
+
+	// our handler will go here
+	workoutHandler := api.NewWorkoutHandler()
+
+	app := &Application{
+		Logger:         logger,
+		WorkoutHandler: workoutHandler,
+		DB:             pgDB,
+	}
+
+	return app, nil
+}
+
+func (a *Application) HealthCheck(w http.ResponseWriter, r *http.Request) {
+	fmt.Fprintf(w, "Status is available\n")
+}
