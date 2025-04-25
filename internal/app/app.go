4f9a9142d4e148d6f5d77193912b706ca39fc5b3
Rajib Aqil Akbar <rajibaqil11@gmail.com>
2025-04-26 02:33:13 +0700

    Add workout module and initial DB migrations
    - Update app, routes, and DB store for workouts
    
    - Add workout store
    
    - Add user, workout, and entry migration files
    
    - Include fs.go for embedded migrations

--- internal/app/app.go
+++ internal/app/app.go
@@ -9,6 +9,7 @@ import (
 
 	"github.com/jivelocity/femGo/internal/api"
 	"github.com/jivelocity/femGo/internal/store"
+	"github.com/jivelocity/femGo/migrations"
 )
 
 type Application struct {
@@ -22,12 +23,19 @@ func NewApplication() (*Application, error) {
 	if err != nil {
 		return nil, err
 	}
+
+	err = store.MigrateFS(pgDB, migrations.FS, ".")
+	if err != nil {
+		panic(err)
+	}
+
 	logger := log.New(os.Stdout, "", log.Ldate|log.Ltime)
 
 	// our store will go here
+	workoutStore := store.NewPostgresWorkoutStore(pgDB)
 
 	// our handler will go here
-	workoutHandler := api.NewWorkoutHandler()
+	workoutHandler := api.NewWorkoutHandler(workoutStore)
 
 	app := &Application{
 		Logger:         logger,
